5a866e8ec595de5975bbbe722ef2b0c77ad43732
Ceci <78119165+Ceci-123@users.noreply.github.com>
2022-04-06 16:33:28 -0300

    terminado

---
+++ Entidades/Estante.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Ubicacion del estante: {e.ubicacionEstante} ");
+            foreach (Producto item in e.productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(item));
+            }
+
+            sb.AppendLine("------------------------------");
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool retorno = false;
+            if (e is not null && p is not null)
+            {
+                foreach (Producto item in e.productos)
+                {
+                    if (item == p)
+                    {
+                        retorno = true;
+                        break;
+                    }
+                }
+            }
+
+
+
+            return retorno;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool retorno = false;
+            if (e is not null && p is not null)
+            {
+                int posicion = CalcularLibre(e);
+
+                if (posicion >= 0 && NoEsta(e,p))
+                {
+
+
+                    e.productos[posicion] = p;
+                    retorno = true;
+
+
+                }
+            }
+
+            return retorno;
+
+        }
+
+        private static bool NoEsta(Estante e, Producto p)
+        {
+            bool retorno = false;
+            if(e != p)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        private static int CalcularLibre(Estante e)
+        {
+            int retorno = -1;
+            if (e is not null)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        retorno = i;
+                        break;
+                    }
+                }
+            }
+
+
+            return retorno;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            Estante estanteRetorno = e;
+            if (e is not null && p is not null)
+            {
+                if (!(e == p))
+                {
+                    for (int i = 0; i < e.productos.Length; i++)
+                    {
+                        if (e.productos[i] == p)
+                        {
+                            e.productos[i] = null;
+                        }
+                    }
+                }
+            }
+
+
+
+            return e;
+        }
+    }
+}
---
+++ Entidades/Producto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string codigoDeBarra, string marca, float precio)
+        {
+            this.codigoDeBarra = codigoDeBarra;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (p1 is not null && p2 is not null)
+            {
+                return p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra;
+
+            }
+            else return false;
+        }
+        public static bool operator ==(Producto p, string marca)
+        {
+            if (p is not null && marca is not null)
+            {
+                return p.marca == marca;
+
+            }
+            else return false;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Marca del producto: {p.marca} Precio del producto: {p.precio}") ;
+            sb.AppendLine($"Codigo de barras: {p.codigoDeBarra}");
+            return sb.ToString();
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+    }
+}
